4337371d656c2b2e102d377a021c837533f14182
censimento-opere-edili <matteo.comai@gmail.com>
2025-06-14 11:58:11 +0200

    col js va meglioo

---
+++ galleria.js
@@ -0,0 +1,79 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const gallery = document.querySelectorAll(".griglia .foto"),
+          previewBox = document.querySelector(".fullimg"),
+          previewImg = previewBox.querySelector("img"),
+          closeIcon = document.querySelector(".fullimg .x"),
+          captionText = document.getElementById("caption"),
+          prevBtn = document.querySelector(".prev"),
+          nextBtn = document.querySelector(".next");
+
+    let currentIndex = 0;
+    let clickImgIndex = 0;
+
+    function preview(index) {
+        const selectedImg = gallery[index].querySelector("img");
+        const thumbSrc = selectedImg.src; // es. fotografie/thumb/1.webp
+        const largeSrc = thumbSrc.replace("/thumb/", "/large/"); // cambio path
+
+        previewImg.src = largeSrc; // uso immagine grande
+        captionText.textContent = selectedImg.alt;
+
+        prevBtn.style.display = (index === 0) ? "none" : "block";
+        nextBtn.style.display = (index >= gallery.length - 1) ? "none" : "block";
+    }
+
+    gallery.forEach((item, i) => {
+        item.onclick = () => {
+            currentIndex = i;
+            clickImgIndex = i;
+            preview(currentIndex);
+            previewBox.classList.add("show");
+        }
+    });
+
+    prevBtn.onclick = () => {
+        if (currentIndex > 0) {
+            currentIndex--;
+            preview(currentIndex);
+        }
+    };
+
+    nextBtn.onclick = () => {
+        if (currentIndex < gallery.length - 1) {
+            currentIndex++;
+            preview(currentIndex);
+        }
+    };
+
+    closeIcon.onclick = () => {
+        previewBox.classList.remove("show");
+        currentIndex = clickImgIndex; 
+    };
+
+    let touchStartX = 0;
+    let touchEndX = 0;
+
+    previewBox.addEventListener('touchstart', (e) => {
+        touchStartX = e.changedTouches[0].screenX;
+    });
+
+    previewBox.addEventListener('touchend', (e) => {
+        touchEndX = e.changedTouches[0].screenX;
+        handleGesture();
+    });
+
+    function handleGesture() {
+        if (touchEndX < touchStartX - 40) {
+            if(currentIndex < gallery.length - 1) {
+                currentIndex++;
+                preview(currentIndex);
+            }
+        }
+        if (touchEndX > touchStartX + 40) {
+            if(currentIndex > 0) {
+                currentIndex--;
+                preview(currentIndex);
+            }
+        }
+    }
+});
